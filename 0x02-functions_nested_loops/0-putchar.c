59fa356ccb24ad46a15e77c36920a75097090a1f
Asma <lualuttle@gmail.com>
2023-07-19 16:36:54 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include 'main.h'
+/**
+   *a program that prints _putchar
+   */
+int main ()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
