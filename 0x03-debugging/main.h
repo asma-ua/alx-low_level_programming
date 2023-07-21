8be2f67e2746726d4c52f3a52d87bc593d8794fe
Asma <lualuttle@gmail.com>
2023-07-20 17:09:19 -0700

    0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,28 +1,8 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- * main - Determines if the number is positive , nagative or zero
- *
- * Return: always 0 (Success)
- */
-int main(void)
-{
-	int n;
+#ifndef MAIN_H
+#define MAIN_H
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
