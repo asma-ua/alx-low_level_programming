356402caf0666d7062020040610448d16e47d900
Asma <lualuttle@gmail.com>
2023-07-28 17:05:27 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,33 @@
 #include "main.h"
 /**
-*leet -  function to concatinate two strings
+*cap_string -  function to concatinate two strings
 *
 *@str: the inputed pointer of array
 *
 *Return: return the length of the array
 */
-char *leet(char *str)
+char *cap_string(char *str)
 {
-int i;
-int c;
-char *s = str;
-for (i = 0; s[i] != '\0'; i++)
+char *p = str;
+int cap = 1;
+while (*p)
 {
-c = s[i];
-if (c == 'a' || c == 'A')
-str[i] = '4';
-else if (c == 'e' || c == 'E')
-str[i] = '3';
-else if (c == 'o' || c == 'O')
-str[i] = '0';
-else if (c == 't' || c == 'T')
-str[i] = '7';
-else if (c == 'l' || c == 'L')
-str[i] = '1';
+if (*p == ' ' || *p == '\t' || *p == '\n' || *p == ','
+|| *p == ';' || *p == '.' ||
+*p == '!' || *p == '?' || *p == '"' || *p ==
+'(' || *p == ')' || *p == '{' || *p == '}')
+{
+cap = 1;
+}
+else if (cap)
+{
+if (*p >= 'a' && *p <= 'z')
+{
+*p -= ('a' - 'A');
+}
+cap = 0;
+}
+p++;
 }
 return (str);
 }
