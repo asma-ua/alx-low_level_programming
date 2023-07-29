b829beaf0e3cc47c0580eac690e7fd2c211a4ae5
Asma <lualuttle@gmail.com>
2023-07-28 17:00:07 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*string_toupper-  function to concatinate two strings
+*
+*@str: the inputed pointer of array
+*
+*Return: return the length of the array
+*/
+char *string_toupper(char *str)
+{
+char *p = str;
+while (*p) {
+if (*p >= 'a' && *p <= 'z') {
+*p = *p - ('a' - 'A');
+}
+p++;
+}
+return (str);
+}
